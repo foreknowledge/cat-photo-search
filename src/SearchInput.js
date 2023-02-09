d8a7f8340462c861b2bdf0eeabd61b4d4febe7cd
foreknowledge <kyj_0129@naver.com>
2023-02-09 16:53:49 +0900

    feat: input 재진입 시 value 초기화

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -10,6 +10,12 @@ class SearchInput {
     $searchInput.className = 'SearchInput';
     $target.appendChild($searchInput);
 
+    $searchInput.addEventListener('focus', (e) => {
+      if (e.target.value) {
+        e.target.value = '';
+      }
+    });
+
     $searchInput.addEventListener('keyup', (e) => {
       if (e.key === 'Enter') {
         onSearch(e.target.value);
